631f2cfec43efc911a3f86548fc6d04177d8f342
MikeyBeez <mbonsign@gmail.com>
2023-12-20 14:06:45 -0600

    added ragtool

---
+++ ollama/RAGtool.py
@@ -0,0 +1,26 @@
+from langchain.llms import Ollama
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import GPT4AllEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+import gradio as gr
+
+def process_url_and_question(url: str, question: str):
+    loader = WebBaseLoader(url)
+    data = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    all_splits = text_splitter.split_documents(data)
+    vectorstore = Chroma.from_documents(documents=all_splits, embedding=GPT4AllEmbeddings())
+
+    ollama = Ollama(base_url="http://localhost:11434", model="llama2")
+    qachain = RetrievalQA.from_chain_type(ollama, retriever=vectorstore.as_retriever())
+
+    result = qachain({"query": question})
+    return result
+
+iface = gr.Interface(fn=process_url_and_question, 
+                     inputs=["text", "text"], 
+                     outputs="text")
+iface.launch()
+
--- ollama/testRAG1.py
+++ ollama/testRAG1.py
@@ -1,4 +1,3 @@
-import re
 from langchain.llms import Ollama
 from langchain.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -6,7 +5,7 @@ from langchain.embeddings import GPT4AllEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 
-loader = WebBaseLoader("https://en.wikipedia.org/wiki/Hawaii")
+loader = WebBaseLoader("https://arxiv.org/pdf/2303.03381.pdf")
 data = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 all_splits = text_splitter.split_documents(data)
@@ -14,7 +13,7 @@ vectorstore = Chroma.from_documents(documents=all_splits, embedding=GPT4AllEmbed
 
 ollama = Ollama(base_url="http://localhost:11434", model="llama2")
 qachain = RetrievalQA.from_chain_type(ollama, retriever=vectorstore.as_retriever())
-question = "When was Hawaii add as a state?"
+question = "What are the top five main ideas in this paper?  Why is this paper important?"
 
 result = qachain({"query": question})
 print(result)
